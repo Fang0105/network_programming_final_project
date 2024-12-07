a5c3c03921c281df5ae49e0956851bbea350d84d
Fang0105 <nody0105@gmail.com>
2024-12-07 17:02:47 +0800

    finish RoomServer()

--- RoomServer.hpp
+++ RoomServer.hpp
@@ -7,22 +7,32 @@
 
 class RoomServer {
     private:
-    sockaddr_in addr;
-    std::vector<int> users_fd;
-    int presenter_fd;
+        sockaddr_in addr;
+        std::vector<int> users_fd;
+        int presenter_fd;
+        int port;
+
+        int accept_user_socket;
 
     public:
-    RoomServer(const std::string& ip, int port) {
+        RoomServer(struct sockaddr_in addr, int port) {
+            accept_user_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    }
+            addr.sin_port = htons(port);
 
-    void AcceptUser() {
+            this->port = port;
+            this->addr = addr;
 
-    }
+            Bind(accept_user_socket, (SA *) &this->addr, sizeof(this->addr));
+        }
 
-    void Boradcast() {
-        
-    }
+        void AcceptUser() {
+
+        }
+
+        void Boradcast() {
+            
+        }
 };
 
 #endif
