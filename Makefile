010830eb338dcdb5157f1299179572d5ef49878c
fayian <ianleetaiwan@outlook.com>
2024-12-07 15:23:37 +0800

    Added RoomServer.hpp

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@ include ../Make.defines
 
 PROGS =	central_server test
 ODIR = output
+INCLUDE = RoomServer.hpp
 
 all:	${PROGS}
 
 central_server:	central_server.c
-		${CC} ${CFLAGS} -o ${ODIR}/$@ $< ${LIBS}
+		${CC} ${CFLAGS} -o ${ODIR}/$@ $< ${INCLUDE} ${LIBS} 
 
 test:	test.c
 		${CC} ${CFLAGS} -o ${ODIR}/$@ $< ${LIBS}
---
+++ RoomServer.hpp
@@ -0,0 +1,28 @@
+#ifndef __FINAL_PROJECT_ROOM_SERVER__
+#define __FINAL_PROJECT_ROOM_SERVER__
+
+#include "unp.h"
+#include <string>
+#include <vector>
+
+class RoomServer {
+    private:
+    sockaddr_in addr;
+    std::vector<int> users_fd;
+    int presenter_fd;
+
+    public:
+    RoomServer(const std::string& ip, int port) {
+
+    }
+
+    void AcceptUser() {
+
+    }
+
+    void Boradcast() {
+        
+    }
+};
+
+#endif
