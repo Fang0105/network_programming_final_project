64b5edbefe13d0d0cc9094400e13663fdbd0cb46
Fang0105 <nody0105@gmail.com>
2024-12-18 18:50:03 +0800

    some structure

--- structures.h
+++ structures.h
@@ -3,6 +3,9 @@
 
 #include <string>
 
+#define LISTENQ 1024
+#define FRAMES_PER_BUFFER 512
+
 enum Identiy {ERROR, AUDIENCE, PROVIDER};
 
 struct UserData {
@@ -14,17 +17,54 @@ struct UserData {
 struct RoomData {
     int room_id;
     std::string room_name;
-    UserData host_user;    
+    UserData host_user;
+    int running_port;
 };
 
 
-void serialize(const UserData &obj, char *buffer) {
+void serialize_UserData(const UserData &obj, char *buffer) {
     memcpy(buffer, &obj, sizeof(UserData));
 }
 
-void deserialize(const char *buffer, UserData &obj) {
+void deserialize_UserData(const char *buffer, UserData &obj) {
     memcpy(&obj, buffer, sizeof(UserData));
 }
 
+enum CommandType {CREATE_ROOM, LIST_ROOM, JOIN_ROOM};
+
+struct Command{
+    CommandType type;
+    UserData user; // store user data for host information
+    int room_id; // for audience to JOIN_ROOM
+    std::string room_name; // for host to CREATE_ROOM
+};
+
+void serialize_Command(const Command &obj, char *buffer) {
+    memcpy(buffer, &obj, sizeof(Command));
+}
+
+void deserialize_Command(const char *buffer, Command &obj) {
+    memcpy(&obj, buffer, sizeof(Command));
+}
+
+void serialize_RoomData(const RoomData &obj, char *buffer) {
+    memcpy(buffer, &obj, sizeof(RoomData));
+}
+
+void deserialize_RoomData(const char *buffer, RoomData &obj) {
+    memcpy(&obj, buffer, sizeof(RoomData));
+}
+
+struct Number {
+    int num;
+};
+
+void serialize_Number(const Number &obj, char *buffer) {
+    memcpy(buffer, &obj, sizeof(Number));
+}
+
+void deserialize_Number(const char *buffer, Number &obj) {
+    memcpy(&obj, buffer, sizeof(Number));
+}
 
 #endif
